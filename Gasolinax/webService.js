82b33ae1d5109e368b1020ac3a2b5b7df9e0f084
adriandrew <adriandrew@hotmail.com>
2014-06-30 11:39:11 -0600

    Perfecto, ya está offline, solo falta hacer ajutes.

--- Gasolinax/webService.js
+++ Gasolinax/webService.js
@@ -3,12 +3,58 @@ $(document).delegate('#Diesel', 'click', calcularDiesel);
 
 $(document).ready(
 
+  llamarFunciones
+
 	// Se llama a la funcion invocarJson.
 	//invocarJson
 
-  obtenerPrecios
+  // Se llama a la funcion obtenerPrecios de cada tipo.
+  //obtenerPrecios('Diesel'),
+  //obtenerPrecios('Magna'),
+  //obtenerPrecios('Premium')
 
 );
+
+function llamarFunciones(){
+
+try {
+    //Block of code to try
+
+    // Se llama a la funcion invocarJson.
+  invocarJson,
+
+    // Se llama a la funcion obtenerPrecios de cada tipo.
+   // Se llama a la funcion obtenerPrecios de cada tipo.
+  obtenerPrecios('Diesel'),
+  obtenerPrecios('Magna'),
+  obtenerPrecios('Premium')
+
+}
+catch(err) {
+    //Block of code to handle errors
+
+    //invocarObtenerPrecios
+
+      // Se llama a la funcion obtenerPrecios de cada tipo.
+  obtenerPrecios('Diesel'),
+  obtenerPrecios('Magna'),
+  obtenerPrecios('Premium')
+
+}
+
+}
+
+
+//TODO. Falló al hacerlo de esta manera u.u
+function invocarObtenerPrecios(){
+
+  // Se llama a la funcion obtenerPrecios de cada tipo.
+  obtenerPrecios('Diesel'),
+  obtenerPrecios('Magna'),
+  obtenerPrecios('Premium')
+
+}
+
 
 function calcularDiesel() {
     //document.getElementById("Diesel").innerHTML = "YOU CLICKED ME!";
@@ -38,11 +84,21 @@ function invocarJson() {
           	window.listadoI = response; 
 		
             	inyectarListado(listadoI)
-          
+
+           // Se llama a la funcion obtenerPrecios de cada tipo.
+  obtenerPrecios('Diesel'),
+  obtenerPrecios('Magna'),
+  obtenerPrecios('Premium')
           },
           
           error: function(XMLHttpRequest, textStatus, Error) {
           
+            // Se llama a la funcion obtenerPrecios de cada tipo.
+  obtenerPrecios('Diesel'),
+  obtenerPrecios('Magna'),
+  obtenerPrecios('Premium')
+
+
           	console.log(XMLHttpRequest, textStatus, Error);
           
           }
@@ -57,30 +113,34 @@ function inyectarListado(listado) {
 
           //$('#inyectarPrecios').append('<h2>'+valor.tipo+': '+valor.importe+'</h2>');
 
-          $('#navPrecios ul').append('<li><a href="calculos.html" id="'+valor.tipo+'">'+valor.tipo+' $'+valor.importe+'</a></li>');
+        //  $('#navPrecios ul').append('<li><a href="calculos.html" id="'+valor.tipo+'">'+valor.tipo+' $'+valor.importe+'</a></li>');
      
-      var tipo = valor.tipo;
-      var importe = valor.importe;
-      localStorage.setItem(tipo, importe);
+        var tipo = valor.tipo;
+        var importe = valor.importe;
+//      localStorage.setItem(tipo, importe);
+
+      guardarPrecios(tipo, importe)
+
+      //obtenerPrecios(tipo)
 
      }); 
 
 } //inyectarListado
 
 
-function guardarPrecios(){
+function guardarPrecios(tipo, importe){
      
-      var tipo = 'Diesel';
-      var importe = '12.3';
+    //  var tipo = 'Diesel';
+    //  var importe = '12.3';
       localStorage.setItem(tipo, importe);
 
 }
 
 
-function obtenerPrecios(){
+function obtenerPrecios(tipo){
 
-     var importe = localStorage.getItem('Diesel') || '<empty>';
-      $('#navPrecios ul').append('<li><a href="calculos.html">'+importe+'</a></li>');
+     var importe = localStorage.getItem(tipo) || '<empty>';
+      $('#navPrecios ul').append('<li><a href="calculos.html">'+tipo+' $ '+importe+'</a></li>');
 
 }
 
