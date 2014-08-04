1e6158f5241f0d90eb0991ff7d1f6261a22cb616
Aldo Adrian Andrew Lopez <aaandrewlopez@gmail.com>
2014-08-04 08:23:24 -0600

    Buenas practicas iOSMEmex..

--- PlantillaMemex/Js/scripts.js
+++ PlantillaMemex/Js/scripts.js
@@ -11,24 +11,16 @@ function Cargada(id){
 
 
 // Muestra Oculta
-/*
 function MuestraOculta(id){
-
-if (document.getElementById){ //se obtiene el id
-var equipos = document.getElementById(id); //se define la variable "equipos" igual a nuestro div
-equipos.style.display = (equipos.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+    if (document.getElementById){ // Se obtiene el id.
+        var equipos = document.getElementById(id); // Se define la variable "equipos" igual a nuestro div.
+        equipos.style.display = (equipos.style.display == 'none') ? 'block' : 'none'; // Damos un atributo display:none que oculta el div.
+    }
 }
 
+window.onload = function(){ // Hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente.
+    MuestraOculta('navEquipos'); // "navEquipos" es el nombre que le dimos al div.
 }
-
-window.onload = function(){//hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente
-
-MuestraOculta('contenido_a_mostrar'); // "contenido_a_mostrar" es el nombre que le dimos al DIV 
-
-}
-
-*/
-
 // Termina Muestra Oculta
 
 // Audio
